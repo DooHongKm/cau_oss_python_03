5eed7eae4f4b69e37c46a74d710e268402a17e84
DooHongKm <127850414+DooHongKm@users.noreply.github.com>
2023-04-04 16:48:06 +0900

    Update simple_calculator.py
    주석 수정

--- simple_calculator.py
+++ simple_calculator.py
@@ -31,11 +31,11 @@ def subtraction(num1, num2):
 while True:
     op = input("input operation:")
     if op == "end":
-        break   # "반복을 종료"
+        break  # "반복을 종료"
     elif op == "+":
-        num1, num2, ret = arithmetic_ops(addition) # 정의된 함수 사용
+        num1, num2, ret = arithmetic_ops(addition)  # 정의된 함수 사용
     elif op == "*":
-        num1, num2, ret = arithmetic_ops(lambda x, y: x*y) # 익명함수(lambda) 사용
+        num1, num2, ret = arithmetic_ops(lambda x, y: x*y)  # 익명함수(lambda) 사용
 
     # [fill this area] 위의 코드를 참고하여 -, /, %에 대한 내용 구현
     elif op == "-":
@@ -46,7 +46,7 @@ while True:
         num1, num2, ret = arithmetic_ops(lambda x, y : x%y)
     else:
         print("Invalid operation")
-        continue # Invalid operation이므로 연산결과를 출력하지 않고 "넘어간다".
+        continue  # Invalid operation이므로 연산결과를 출력하지 않고 "넘어간다".
     print(f"{num1}{op}{num2} = {ret}")  # 연산 결과를 출력
 
 print("Exit program")
