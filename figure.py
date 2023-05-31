395562372b79e31229b039ed0820cacede587dab
DooHongKm <rlaenghdrlae@naver.com>
2023-05-31 21:35:10 +0900

    implement hw5

--- figure.py
+++ figure.py
@@ -2,34 +2,34 @@ import math
 
 class line:
     
-    # 외부에서 접근 불가능한 필드 __length, 초기값 = 0
-    # 생성자를 통해 초기 __length의 값을 지정할 수 있음
+    # 외부에서 접근 불가능한 필드 __width와 __height, 초기값 = 0
+    # 생성자를 통해 초기 __width와 __height의 값을 지정할 수 있음
     __width = 0
     __height = 0
     def __init__(self, width, height):
         self.__width = width
         self.__height = height
         
-    # set_length를 통해 필드에 접근하여 length 값을 바꿀 수 있음
+    # set_length를 통해 필드에 접근하여 width, height 값을 바꿀 수 있음
     def set_length(self, width, height):
         self.__width = width
         self.__height = height
         
-    # get_length를 통해 필드에 접근하여 length 값을 받을 수 있음
+    # get_length를 통해 필드에 접근하여 width, height 값을 받을 수 있음
     def get_length(self):
         return self.__width, self.__height
 
-# 인자로 length를 받아 (length^2)를 반환하는 함수  
+# 인자로 width, height를 받아 (width * height)를 반환하는 함수  
 def area_rectangle(width, height):
     if width <= 0 or height <= 0: raise ValueError
     return width * height
 
-# 인자로 length를 받아 (length^2 * pi)를 반환하는 함수
+# 인자로 width, height를 받아 (width * height * pi)를 반환하는 함수
 def area_ellipse(width, height):
     if width <= 0 or height <= 0: raise ValueError
     return width * height * math.pi
 
-# 인자로 length를 받아 (length^2 * root(3) / 4)를 반환하는 함수
+# 인자로 width, height를 받아 (width * height / 2)를 반환하는 함수
 def area_right_triangle(width, height):
     if width <= 0 or height <= 0: raise ValueError
     return width * height / 2
--- figure_main.py
+++ figure_main.py
@@ -1,26 +1,35 @@
 import figure
 
+# width=10, height=20인 line 객체 생성
 myline = figure.line(10, 20)
 width, height = myline.get_length()
 
+# area_rectangle 함수를 이용하여 직사각형 넓이 출력
+# width, height가 음수인 경우, ValueError가 발생하여 경고문 출력
 try:
     rectangle = figure.area_rectangle(width, height)
     print(rectangle)
 except ValueError:
     print("please input positive number for width and height")
 
+# width=20, height=30으로 변경
 myline.set_length(20, 30)
 width, height = myline.get_length()
 
+# area_right_triangle 함수를 이용하여 직각삼각형 넓이 출력
+# width, height가 음수인 경우, ValueError가 발생하여 경고문 출력
 try:
     triangle = figure.area_right_triangle(width, height)
     print(triangle)
 except ValueError:
     print("please input positive number for width and height")
 
+# width=30, height=40으로 변경
 myline.set_length(30, 40)
 width, height = myline.get_length()
 
+# area_ellipse 함수를 이용하여 타원 넓이 출력
+# width, height가 음수인 경우, ValueError가 발생하여 경고문 출력
 try:
     ellipse = figure.area_ellipse(width, height)
     print(ellipse)
